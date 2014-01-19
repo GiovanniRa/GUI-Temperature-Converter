da69e60ffe16f72ba6f80b270a79218f0fe8dc31
GiovanniRa <GiovanniRa@users.noreply.github.com>
2014-01-19 12:07:37 +0100

    Create convert.py

---
+++ convert.py
@@ -0,0 +1,9 @@
+# The file contains the two basic formulas to convert.
+
+# convert from Fahrenheit to Celsius
+def conv_C(n):
+    return (n - 32) / 1.8
+
+# convert from Celsius to Fahrenheit
+def conv_F(n):
+    return (n * 1.8) + 32
